a4484468723184c7cbc162c9be5d308310bffce0
Ansiba tk <your.email@example.com>
2024-09-04 19:19:39 +0530

    created a program display even numbers using function

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function getEvenNum(n)
+                  {
+                    for(let i=0;i<n;i++)
+                    {
+                      if(i%2==0)
+                       {
+                         console.log(i);
+                       }
+                      
+                    }
+                    }
+
+ getEvenNum(11);
+                  
